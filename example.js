88dcad4d1b0807cddd6329141dd7385bd0b0aa83
Nate Goldman <nate@ngoldman.me>
2016-02-14 11:39:06 -0800

    add example node script

---
+++ example.js
@@ -0,0 +1,33 @@
+var bg = require('./')
+
+bg({
+  /**
+   * The location to save the resulting image.
+   * Default: `~/Pictures/himawari-images/${Date.now()}.jpg`
+   * @type {String}
+   */
+  outfile: './example.jpg',
+
+  /**
+   * The time of the picture desired. If you want to get the latest image, use 'latest'.
+   * Default: 'latest'
+   * @type {String|Date}
+   */
+  date: new Date(2016, 1, 1), // Or new Date() or a date string
+
+  /**
+   * The zoom level of the image. Can be 1-5 (default: 2)
+   * Each zoom level requires more images to be downloaded and therefore stitched
+   * together. Higher zoom yields a higher resolution image.
+   * Default: 2
+   * @type {Number}
+   */
+  zoom: 1,
+
+  /**
+   * If set to true, an image on the infrared light spectrum will be generated
+   * Default: false
+   * @type {Boolean}
+   */
+  infrared: true
+})
